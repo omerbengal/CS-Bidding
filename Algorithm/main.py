d84f63e4f658a105b9a25567bf08f0354a5c210a
Omer Bengal <omer.bengal01@post.runi.ac.il>
2024-07-12 18:34:51 +0300

    refactor: styling

--- Algorithm/main.py
+++ Algorithm/main.py
@@ -3,12 +3,14 @@ from dataclasses import dataclass, field
 from typing import List, Dict, Tuple
 from abc import ABC
 
-
+# Language codes
 HEBREW_LANGUAGE_CODE = 1
 ENGLISH_LANGUAGE_CODE = 2
 
+# Degree codes
 FIRST_DEGREE_CODE = 1
 SECOND_DEGREE_CODE = 2
+SECOND_DEGREE_MLDS_CODE = 22
 ADVANCED_DEGREE_CODE = 3
 
 
@@ -29,7 +31,7 @@ ADVANCED_DEGREE_CODE = 3
     TODO: if student is English and enrolled in Hebrew course - subtract 1 from general "1st" capacity AND from English capacity
     ✅
 
-- new DegreeType: "MLDS" - they should be able to enroll in courses with degreeType "2nd" (as well as "MLDS" of course)
+✅ - new DegreeType: "MLDS" - they should be able to enroll in courses with degreeType "2nd" (as well as "MLDS" of course)
 
 - 2nd round for hebrew students who got rejected in the first round for English courses (due to english capacity being more than the actual english students who enrolled)
 
@@ -42,7 +44,7 @@ ADVANCED_DEGREE_CODE = 3
 
 
 """
-Student's degree 3 should maybe not be MLDS... wait for Tami's answer
+✅ Student's degree 3 should maybe not be MLDS... wait for Tami's answer
 """
 
 
@@ -50,25 +52,25 @@ Student's degree 3 should maybe not be MLDS... wait for Tami's answer
 class Course(ABC):
     code: int
     name: str
-    allowedDegree: int  # 1 - 1st, 2 - 2nd, 3 - advanced [1st and 2nd]
-    type: str  # theoretical or practical
-    capacity: int  # capacity of the course
+    allowedDegree: int  # 1 - 1st, 2 - 2nd, 22 - 2nd MLDS, 3 - advanced [1st and 2nd] # nopep8
+    type: str  # Theoretical or practical
+    capacity: int  # Capacity of the course
 
 
 @dataclass
 class HebrewCourse(Course):
-    englishCapacity: int  # capacity saved for English students
-
-
-@dataclass
-class EnglishCourse(Course):
     pass
 
 
 @dataclass
+class EnglishCourse(Course):
+    englishCapacity: int  # Capacity saved for English students
+
+
+@dataclass
 class Constraint:
-    courses_codes: List[int]  # list of courses codes
-    counter: int  # number of courses from this constraint
+    courses_codes: List[int]  # List of courses codes
+    counter: int  # Number of courses from this constraint
 
 
 @dataclass
@@ -76,26 +78,36 @@ class Student:
     id: int
     name: str
     email: str
-    degree: int  # 1 - 1st, 2 - 2nd, 3 - MLDS
+    degree: int  # 1 - 1st, 2 - 2nd, 22 - 2nd MLDS, 3 - advanced [1st and 2nd]
     language: int  # 1 - Hebrew, 2 - English
-    preferences_courses_codes: List[int]  # list of courses codes - the student's preferences - ordered by priority # nopep8
-    desired_courses_number: int  # number of courses the student wants to enroll in
+    preferences_courses_codes: List[int]  # List of courses codes - the student's preferences - ordered by priority # nopep8
+    desired_courses_number: int  # Number of courses the student wants to enroll in
     enrolled_courses: List[int] = field(default_factory=list)
     constraints: List[Constraint] = field(default_factory=list)
 
 
 def createStudents():
     students = [
-        Student(id=1, name='student1', email='student1@post.runi.ac.il', degree=FIRST_DEGREE_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[1, 2, 3, 4], desired_courses_number=2),  # nopep8
-        Student(id=2, name='student2', email='student2@post.runi.ac.il', degree=SECOND_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[4, 3, 2, 1], desired_courses_number=3),  # nopep8
-        Student(id=3, name='student3', email='student3@post.runi.ac.il', degree=ADVANCED_DEGREE_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[1, 2, 3, 4], desired_courses_number=1),  # nopep8
-        Student(id=4, name='student4', email='student4@post.runi.ac.il', degree=FIRST_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[1, 3, 4, 2], desired_courses_number=2),  # nopep8
-        Student(id=5, name='student5', email='student5@post.runi.ac.il', degree=ADVANCED_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[1, 2, 3, 4], desired_courses_number=1),  # nopep8
-        Student(id=6, name='student6', email='student6@post.runi.ac.il', degree=SECOND_DEGREE_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[3, 2], desired_courses_number=1),  # nopep8
-        Student(id=7, name='student7', email='student7@post.runi.ac.il', degree=FIRST_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[3, 1, 4, 2], desired_courses_number=2),  # nopep8
-        Student(id=8, name='student8', email='student8@post.runi.ac.il', degree=ADVANCED_DEGREE_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[2, 1, 3, 4], desired_courses_number=1),  # nopep8
-        Student(id=9, name='student9', email='student9@post.runi.ac.il', degree=SECOND_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[2, 4, 1, 3], desired_courses_number=2),  # nopep8
-        Student(id=10, name='student10', email='student10@post.runi.ac.il', degree=ADVANCED_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[1, 2, 3, 4], desired_courses_number=1),  # nopep8
+        Student(id=1, name='student1', email='student1@post.runi.ac.il', degree=FIRST_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[3, 2, 4, 1], desired_courses_number=4),  # nopep8
+        Student(id=2, name='student2', email='student2@post.runi.ac.il', degree=SECOND_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[3], desired_courses_number=1),  # nopep8
+        Student(id=3, name='student3', email='student3@post.runi.ac.il', degree=FIRST_DEGREE_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[2, 4, 3, 1], desired_courses_number=4),  # nopep8
+        Student(id=4, name='student4', email='student4@post.runi.ac.il', degree=SECOND_DEGREE_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[2, 1, 3], desired_courses_number=3),  # nopep8
+        Student(id=5, name='student5', email='student5@post.runi.ac.il', degree=SECOND_DEGREE_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[1, 3], desired_courses_number=1),  # nopep8
+        Student(id=6, name='student6', email='student6@post.runi.ac.il', degree=ADVANCED_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[4, 2, 1], desired_courses_number=2),  # nopep8
+        Student(id=7, name='student7', email='student7@post.runi.ac.il', degree=ADVANCED_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[1, 3, 4, 2], desired_courses_number=3),  # nopep8
+        Student(id=8, name='student8', email='student8@post.runi.ac.il', degree=ADVANCED_DEGREE_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[3, 4, 2], desired_courses_number=1),  # nopep8
+        Student(id=9, name='student9', email='student9@post.runi.ac.il', degree=SECOND_DEGREE_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[2, 3, 1], desired_courses_number=2),  # nopep8
+        Student(id=10, name='student10', email='student10@post.runi.ac.il', degree=SECOND_DEGREE_MLDS_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[4, 1, 3], desired_courses_number=2),  # nopep8
+        Student(id=11, name='student11', email='student11@post.runi.ac.il', degree=FIRST_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[4], desired_courses_number=1),  # nopep8
+        Student(id=12, name='student12', email='student12@post.runi.ac.il', degree=SECOND_DEGREE_MLDS_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[2, 4, 1, 3], desired_courses_number=4),  # nopep8
+        Student(id=13, name='student13', email='student13@post.runi.ac.il', degree=SECOND_DEGREE_MLDS_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[4, 3, 2, 1], desired_courses_number=3),  # nopep8
+        Student(id=14, name='student14', email='student14@post.runi.ac.il', degree=ADVANCED_DEGREE_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[2, 1, 4], desired_courses_number=1),  # nopep8
+        Student(id=15, name='student15', email='student15@post.runi.ac.il', degree=SECOND_DEGREE_MLDS_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[2, 4, 3], desired_courses_number=2),  # nopep8
+        Student(id=16, name='student16', email='student16@post.runi.ac.il', degree=SECOND_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[1], desired_courses_number=1),  # nopep8
+        Student(id=17, name='student17', email='student17@post.runi.ac.il', degree=ADVANCED_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[4, 3], desired_courses_number=2),  # nopep8
+        Student(id=18, name='student18', email='student18@post.runi.ac.il', degree=FIRST_DEGREE_CODE, language=ENGLISH_LANGUAGE_CODE, preferences_courses_codes=[1, 4, 3, 2], desired_courses_number=1),  # nopep8
+        Student(id=19, name='student19', email='student19@post.runi.ac.il', degree=SECOND_DEGREE_MLDS_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[4, 3, 2, 1], desired_courses_number=3),  # nopep8
+        Student(id=20, name='student20', email='student20@post.runi.ac.il', degree=SECOND_DEGREE_MLDS_CODE, language=HEBREW_LANGUAGE_CODE, preferences_courses_codes=[3, 1, 4, 2], desired_courses_number=2)  # nopep8
     ]
     return students
 
@@ -112,10 +124,10 @@ def auto_constraint(students: List[Student], courses: List[Course]):
 
 def createCourses():
     courses = [
-        HebrewCourse(code=1, name='course1', allowedDegree=1, type='theoretical', capacity=2),  # nopep8
-        EnglishCourse(code=2, name='course2', allowedDegree=2, type='practical', capacity=2, englishCapacity=2),  # nopep8
-        HebrewCourse(code=3, name='course3', allowedDegree=3, type='theoretical', capacity=2),  # nopep8
-        HebrewCourse(code=4, name='course4', allowedDegree=2, type='theoretical', capacity=2),  # nopep8
+        HebrewCourse(code=1, name='course1', allowedDegree=1, type='theoretical', capacity=5),  # nopep8
+        EnglishCourse(code=2, name='course2', allowedDegree=2, type='practical', capacity=5, englishCapacity=4),  # nopep8
+        HebrewCourse(code=3, name='course3', allowedDegree=3, type='theoretical', capacity=4),  # nopep8
+        HebrewCourse(code=4, name='course4', allowedDegree=2, type='theoretical', capacity=6),  # nopep8
     ]
     return courses
 
@@ -131,23 +143,24 @@ def bidding_course_match(students: List[Student], courses: List[Course]) -> Tupl
 
     courses_dict: Dict[int, Course] = {course.code: course for course in courses}  # nopep8
     L: List[Student] = random.sample(students, len(students))
-    students_id_list = [student.id for student in L]
-    print(f"Students' ids: {students_id_list}")
+
+    id_in_order = [student.id for student in L]
+    print(f"Students' ids acording to the random order: {id_in_order}")
 
     while L:
         next_round_students = []
 
         for student in L:
-            print("student", student.id)
             got_enrolled = False
 
             for preferred_course_code in list(student.preferences_courses_codes):
-                print("course", preferred_course_code)
 
                 # Make sure the student's degree is allowed to enroll in the course
                 if courses_dict[preferred_course_code].allowedDegree != student.degree:
-                    student.preferences_courses_codes.remove(preferred_course_code)  # nopep8
-                    continue
+                    # If the student is 2nd MLDS and the course is 2nd, DO NOT remove the course from the student's preferences list
+                    if not (courses_dict[preferred_course_code].allowedDegree == SECOND_DEGREE_CODE and student.degree == SECOND_DEGREE_MLDS_CODE):
+                        student.preferences_courses_codes.remove(preferred_course_code)  # nopep8
+                        continue
 
                 # Check if there exists a constraint that contains the preferred course and that the number in this constraint is 0 # nopep8
                 constraint_with_course_and_number_is_zero_exists = False
@@ -156,44 +169,52 @@ def bidding_course_match(students: List[Student], courses: List[Course]) -> Tupl
                         constraint_with_course_and_number_is_zero_exists = True
                         break
 
-                # if the student has a constraint that contains the preferred course and the number in this constraint is 0, he can't be enrolled in this course # nopep8
+                # If the student has a constraint that contains the preferred course and the number in this constraint is 0, he can't be enrolled in this course # nopep8
                 if constraint_with_course_and_number_is_zero_exists:
                     student.preferences_courses_codes.remove(preferred_course_code)  # nopep8
                     continue
 
-                # check if the course has capacity
+                # Check if the course has capacity
                 if courses_dict[preferred_course_code].capacity > 0:
-                    # decrease the capacity of the course by 1
+
+                    """HORRAY - the student can be enrolled in the course!"""
+
+                    # Decrease the general capacity of the course by 1
                     courses_dict[preferred_course_code].capacity -= 1
-                    # decrease the English capacity of the Hebrew course by 1 if the student is an English student
-                    if courses_dict[preferred_course_code] is HebrewCourse and student.language == ENGLISH_LANGUAGE_CODE:
+
+                    # If the student is an English student - decrease the English capacity of the English course by 1
+                    if courses_dict[preferred_course_code] is EnglishCourse and student.language == ENGLISH_LANGUAGE_CODE:
                         courses_dict[preferred_course_code].englishCapacity -= 1
-                    # decrease the number in the constraints that contains the preferred course # nopep8
+
+                    # Decrease the number in the constraints that contains the preferred course
                     for constraint in student.constraints:
                         if preferred_course_code in constraint.courses_codes:
                             constraint.counter -= 1
-                    # enroll the student in the course
+
+                    # Enroll the student in the course
                     student.enrolled_courses.append(preferred_course_code)
-                    # remove the course from the student's preferences list
+
+                    # Remove the course from the student's preferences list
                     student.preferences_courses_codes.remove(preferred_course_code)  # nopep8
                     got_enrolled = True
-                    print("student", student.id, "enrolled in", preferred_course_code)  # nopep8
                     break
 
-            # if the student needs to be enrolled in more courses and he has more courses to enroll in # nopep8
+            # If the student needs to be enrolled in more courses and he has more courses to enroll in # nopep8
             if len(student.enrolled_courses) < student.desired_courses_number and student.preferences_courses_codes:
-                # if the student got enrolled in a course, add him to the next round
+
+                # If the student got enrolled in a course, add him to the next round
                 if got_enrolled:
                     next_round_students.append(student)
-                # if the student didn't get enrolled in any course, add him to the next round
+
+                # If the student didn't get enrolled in any course, add him to the next round
                 else:
                     print(f"Student {student.id} couldn't be enrolled in any more courses.")  # nopep8
 
-        # if L is the same as next_round_students, break the loop (because nothing changed)
+        # If L is the same as next_round_students, break the loop (because nothing changed)
         if next_round_students == L:
             break
 
-        # put in L the reversed next_round_students
+        # Put in L the reversed next_round_students
         L = list(reversed(next_round_students))
 
     return students, courses_dict
@@ -205,13 +226,12 @@ def main():
 
     enrolled_students, courses_after_enrollment = bidding_course_match(students, courses)  # nopep8
 
-    # print("Enrolled students:")
-    # for student in enrolled_students:
-    #     print(f"{student.name}: {student.enrolled_courses}")
-
-    # print("Courses' capacities:")
-    # for course in courses_after_enrollment.values():
-    #     print(f"{course.name}: {course.capacity}")
+    for course in courses_after_enrollment.values():
+        students = []
+        for student in enrolled_students:
+            if course.code in student.enrolled_courses:
+                students.append(student)
+        print(f"{course.name}: {[student.id for student in students]}")
 
 
 if __name__ == '__main__':
